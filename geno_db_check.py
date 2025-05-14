cd7519a69860b46c509d5f83a299a74435a7ced7
Kate Howell <kate.howell@ukhsa.gov.uk>
2025-05-14 13:54:47 +0100

    move functions to modules

--- geno_db_check.py
+++ geno_db_check.py
@@ -1,26 +1,14 @@
 import os
-import glob
 import sys
 import pandas as pd
-import subprocess
-from pathlib import Path
-import uuid
-import shutil
 import logging
-import sqlalchemy
-from sqlalchemy import (
-    select,
-    MetaData,
-    inspect,
-    and_,
-    URL,
-    func
-)
-
+from sqlalchemy import inspect, MetaData, func,select
 import argparse
 
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+import src.utilities as util 
+
 
 now = date.today()
 x_int = int(now.strftime("%Y%m%d%H%M%S"))
@@ -28,37 +16,7 @@ x_int = int(now.strftime("%Y%m%d%H%M%S"))
 __version__ = 0.1
 __author__ = 'Kate Howell'
 
-def logging_file_setup(output_folder):
-    """
-    Log file setup in output folder
 
-    :return: None
-    """
-    print("Setting up logging information for debugging.")
-    # Need to clean up log files, currently generates multiple log files.
-    datetime_stamp = now.strftime("%Y%m%d-%H%M%S")
-    logging_file_output = os.path.join(output_folder, str(str(now.strftime("%Y%m%d")) + str('_geno_id_check.log')))
-    print("Logging output in the file: {}".format(logging_file_output))
-    logging.basicConfig(
-            filename=logging_file_output,  # filename=logging_file_output,
-            filemode='w',
-            format='%(asctime)s:%(levelname)s:%(message)s',
-            level=logging.INFO)
-    print("Logging Level: INFO")
-    logging.info("Logging Started.")
-    logging.info(f"Version: {__version__}")
-
-
-def get_aiseqdb_conn():
-    return sqlalchemy.create_engine(
-        URL.create(
-            "postgresql+psycopg2",
-            username=os.environ["AISEQDB_PROD_USER"],
-            password=os.environ["AISEQDB_PROD_PASSWORD"],
-            host=os.environ["AISEQDB_PROD_HOST"],
-            database=os.environ["AISEQDB_PROD_DB"]
-        )
-    )
 
 
 def read_commandline():
@@ -84,18 +42,6 @@ def read_commandline():
 
     return args
 
-def check_arguments(args):
-    """
-    Check that paths provided exist and create output folder if it doesn't exist already.
-
-    :param args: output from arg parse
-    :return: status
-    """
-    if not os.path.isdir(args.output_dir):
-        os.mkdir(args.output_dir)
-    return 0
-
-
 
 def main(args):
     start_time = datetime.now()  # Start time for calculating performance improvements
@@ -107,9 +53,9 @@ def main(args):
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
     # Set up logging
-    logging_file_setup(output_dir)
+    util.logging_file_setup(output_dir)
 
-    db_conn = get_aiseqdb_conn()
+    db_conn = util.get_aiseqdb_conn()
 
     #Set up aiseqdb connection and tables
     insp = inspect(db_conn)
@@ -172,8 +118,8 @@ def main(args):
 if __name__ == '__main__':
     args = read_commandline()
     # Setting up testing and logging
-    logging_file_setup(args.output_dir)
-    check = check_arguments(args)
+    util.logging_file_setup(args.output_dir)
+    check = util.check_arguments(args)
     if check == 1:
         sys.exit(logging.error("Arguments provided were not expected. Please check log."))
     
